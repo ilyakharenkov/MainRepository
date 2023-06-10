28be3c9420df4a0487ab0de2463bba2b0840d2e8
ilyakharenkov <ilyakharenkov@gmail.com>
2023-06-10 15:46:51 +0300

    response categories

--- DeliveryProjectTest/app/src/main/java/com/example/deliveryprojecttest/presentation/screens/CategoryScreenFragment.kt
+++ DeliveryProjectTest/app/src/main/java/com/example/deliveryprojecttest/presentation/screens/CategoryScreenFragment.kt
@@ -2,10 +2,8 @@ package com.example.deliveryprojecttest.presentation.screens
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
 import com.example.deliveryprojecttest.R
 import com.example.deliveryprojecttest.databinding.FragmentCategoryScreenBinding
 
